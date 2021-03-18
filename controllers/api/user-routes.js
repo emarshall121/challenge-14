bd9dc0740d6b7324ad376ebf5561150e14df4330
EMarshall <emilyjmarshall121@gmail.com>
2021-03-18 13:43:16 -0700

    added post model

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -49,6 +49,29 @@ router.post('/', (req, res) => {
     });
 });
 
+// Login Route
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
 // Update a user
 router.put('/:id', (req, res) => {
   User.update(req.body, {
