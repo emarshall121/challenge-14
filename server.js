30f5d5f7cbb6de8b45b633d7b2b6e4dfa4f96009
EMarshall <emilyjmarshall121@gmail.com>
2021-03-18 21:07:14 -0700

    homepage setup

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up of the template engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
